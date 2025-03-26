a808970f751c4534236211297987bf9f71fef9b3
Krishnanmalli <mallikrishnan125@gmail.com>
2025-03-26 19:53:56 +0530

    Add files via upload

---
+++ RecordRetrieval.java
@@ -0,0 +1,40 @@
+package com.project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class RecordRetrieval {
+
+	public static void main(String[] args) {
+        WebDriver driver;
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		options.setHeadless(false);
+		driver = new ChromeDriver(options);
+		
+		driver.manage().window().maximize();	
+		driver.get("https://demo.openmrs.org/openmrs/login.htm");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.id("password")).sendKeys("Admin123");
+		driver.findElement(By.id("Registration Desk")).click();
+		driver.findElement(By.id("loginButton")).click();
+		driver.findElement(By.id("coreapps-activeVisitsHomepageLink-coreapps-activeVisitsHomepageLink-extension")).click();
+		driver.findElement(By.id("patient-search")).sendKeys("Paul Walker");
+		driver.findElement(By.className("odd")).click();
+		driver.navigate().back();
+        driver.findElement(By.id("patient-search")).sendKeys("Mary Miller");
+        driver.findElement(By.className("odd")).click();
+        driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a")).click();
+        driver.findElement(By.className("nav-item logout")).click();
+	}
+}
+
+
---
+++ VitalRecordEntry.java
@@ -0,0 +1,51 @@
+package com.project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+ 
+public class VitalRecordEntry {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		options.setHeadless(false);
+		driver = new ChromeDriver(options);
+		
+		driver.manage().window().maximize();	
+		driver.get("https://demo.openmrs.org/openmrs/login.htm");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("Admin123");
+		driver.findElement(By.id("Registration Desk")).click();
+		driver.findElement(By.id("loginButton")).click();
+		driver.findElement(By.id("referenceapplication-vitals-referenceapplication-vitals-extension")).click();
+		driver.findElement(By.id("patient-search")).sendKeys("100J19");
+		driver.findElement(By.className("odd")).click();
+		driver.findElement(By.className("icon-arrow-right")).click();
+		driver.findElement(By.id("w8")).sendKeys("170");
+		driver.findElement(By.id("next-button")).click();
+		driver.findElement(By.id("w10")).sendKeys("75");
+		driver.findElement(By.id("next-button")).click();
+		driver.findElement(By.id("next-button")).click();
+		driver.findElement(By.id("w12")).sendKeys("40");
+		driver.findElement(By.id("next-button")).click();
+        driver.findElement(By.id("w14")).sendKeys("85");
+		driver.findElement(By.id("next-button")).click();
+		driver.findElement(By.id("w16")).sendKeys("15");
+		driver.findElement(By.id("next-button")).click();
+		driver.findElement(By.id("w18")).sendKeys("120");
+		driver.findElement(By.id("w20")).sendKeys("80");
+		driver.findElement(By.id("next-button")).click();
+		driver.findElement(By.id("w22")).sendKeys("98");
+		driver.findElement(By.id("next-button")).click();
+		driver.findElement(By.xpath("//*[@id=\"confirmationQuestion\"]/p[1]/button")).click();
+
+	}
+
+}
