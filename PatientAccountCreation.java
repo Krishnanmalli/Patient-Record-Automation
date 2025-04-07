ae0e37cbcdc8442d1c3c45a65810eaeaab1a0f1f
Krishnanmalli <mallikrishnan125@gmail.com>
2025-04-07 22:08:02 +0530

    Update PatientAccountCreation.java

--- PatientAccountCreation.java
+++ PatientAccountCreation.java
@@ -9,15 +9,12 @@ import org.openqa.selenium.support.ui.Select;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PatientAccountCreation {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args){
 		WebDriver driver;
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		options.setHeadless(false);
 		driver = new ChromeDriver(options);
-		
 		driver.manage().window().maximize();	
 		driver.get("https://demo.openmrs.org/openmrs/login.htm");
 		driver.findElement(By.id("username")).sendKeys("admin");
@@ -25,32 +22,31 @@ public class PatientAccountCreation {
 		driver.findElement(By.id("Registration Desk")).click();
 		driver.findElement(By.id("loginButton")).click();
 		driver.findElement(By.id("referenceapplication-registrationapp-registerPatient-homepageLink-referenceapplication-registrationapp-registerPatient-homepageLink-extension")).click();
-		driver.findElement(By.name("givenName")).sendKeys("Krishnan");
-		driver.findElement(By.name("familyName")).sendKeys("Malli");
+		driver.findElement(By.name("givenName")).sendKeys("Michael");
+		driver.findElement(By.name("familyName")).sendKeys("Carter");
 		driver.findElement(By.id("next-button")).click();
+		Select genderfield = new Select(driver.findElement(By.id("gender-field")));
+		genderfield.selectByValue("M");
 		driver.findElement(By.xpath("//*[@id=\"gender-field\"]/option[1]")).click(); 
 		driver.findElement(By.id("next-button")).click();
-		driver.findElement(By.id("birthdateYears-field")).sendKeys("21");
-		driver.findElement(By.id("birthdateMonths-field")).sendKeys("3");
+		driver.findElement(By.id("birthdateDay-field")).sendKeys("5");
+		Select birthdaymonth = new Select(driver.findElement(By.id("birthdateMonth-field")));
+		birthdaymonth.selectByValue("2");
+		driver.findElement(By.id("birthdateYear-field")).sendKeys("1996");
 		driver.findElement(By.id("next-button")).click();
-		driver.findElement(By.id("address1")).sendKeys("1/1 Murugan Street");
-		driver.findElement(By.id("cityVillage")).sendKeys("Chennai");
-		driver.findElement(By.id("stateProvince")).sendKeys("Tamilnadu");
-		driver.findElement(By.id("country")).sendKeys("India");
-		driver.findElement(By.id("postalCode")).sendKeys("600011");
+		driver.findElement(By.id("address1")).sendKeys("742 Evergreen Terrace");
+		driver.findElement(By.id("cityVillage")).sendKeys("Springfield");
+		driver.findElement(By.id("stateProvince")).sendKeys("IL");
+		driver.findElement(By.id("country")).sendKeys("USA");
+		driver.findElement(By.id("postalCode")).sendKeys("62704");
 		driver.findElement(By.id("next-button")).click();
-		driver.findElement(By.name("phoneNumber")).sendKeys("9874562310");
+		driver.findElement(By.name("phoneNumber")).sendKeys("+1 (312) 774-9283");
 		driver.findElement(By.id("next-button")).click();
 		Select relation = new Select(driver.findElement(By.id("relationship_type")));
-		relation.selectByIndex(1);
-		Thread.sleep(2000);
-		relation.selectByValue("8d91a01c-c2cc-11de-8d13-0010c6dffd0f-A");
-		Thread.sleep(2000);
 		relation.selectByVisibleText("Parent");
-		Thread.sleep(2000);
-		driver.findElement(By.className("person-typeahead")).sendKeys("anbu");
+		driver.findElement(By.className("person-typeahead")).sendKeys("Miller");
 		driver.findElement(By.id("next-button")).click();
 		driver.findElement(By.id("submit")).click();
+		driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a")).click();
 	}
 }
-
