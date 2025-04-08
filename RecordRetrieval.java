ddb9f8c8a454aff719e19f357a279d539a1009a4
Krishnanmalli <mallikrishnan125@gmail.com>
2025-04-08 23:16:12 +0530

    Update RecordRetrieval.java

--- RecordRetrieval.java
+++ RecordRetrieval.java
@@ -1,16 +1,18 @@
 package com.project;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class RecordRetrieval {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
                 WebDriver driver;
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
@@ -22,14 +24,12 @@ public class RecordRetrieval {
 		driver.findElement(By.id("Registration Desk")).click();
 		driver.findElement(By.id("loginButton")).click();
 		driver.findElement(By.id("coreapps-activeVisitsHomepageLink-coreapps-activeVisitsHomepageLink-extension")).click();
-		driver.findElement(By.id("patient-search")).sendKeys("Paul Walker");
-		driver.findElement(By.className("odd")).click();
-		driver.navigate().back();
-                driver.findElement(By.id("patient-search")).sendKeys("Mary Miller");
-                driver.findElement(By.className("odd")).click();
-                driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a")).click();
-                driver.findElement(By.className("nav-item logout")).click();
+		driver.findElement(By.id("patient-search")).sendKeys("Michael Green");
+		driver.findElement(By.xpath("//*[@id=\"patient-search-results-table\"]/tbody/tr[1]/td[1]")).click();
+		WebElement apbutton = driver.findElement(By.xpath("//*[@id=\"content\"]/div[9]/div[1]/div/div[1]/div[6]/div[1]/a/i"));
+		((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", apbutton);
+		Thread.sleep(1000);
+                driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a/i")).click();
+		driver.quit();
 	}
 }
-
-
