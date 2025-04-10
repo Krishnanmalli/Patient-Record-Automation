66929a4a980a89845b5c2f40edbbc28883bc25a8
Krishnanmalli <mallikrishnan125@gmail.com>
2025-04-10 19:56:30 +0530

    Update RecordRetrieval.java

--- RecordRetrieval.java
+++ RecordRetrieval.java
@@ -3,7 +3,6 @@ package com.project;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -24,12 +23,14 @@ public class RecordRetrieval {
 		driver.findElement(By.id("Registration Desk")).click();
 		driver.findElement(By.id("loginButton")).click();
 		driver.findElement(By.id("coreapps-activeVisitsHomepageLink-coreapps-activeVisitsHomepageLink-extension")).click();
-		driver.findElement(By.id("patient-search")).sendKeys("Michael Green");
+		driver.findElement(By.id("patient-search")).sendKeys("Michael Johnson");
 		driver.findElement(By.xpath("//*[@id=\"patient-search-results-table\"]/tbody/tr[1]/td[1]")).click();
-		WebElement apbutton = driver.findElement(By.xpath("//*[@id=\"content\"]/div[9]/div[1]/div/div[1]/div[6]/div[1]/a/i"));
-		((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", apbutton);
+		Thread.sleep(2000);
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,500)");
 		Thread.sleep(1000);
                 driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a/i")).click();
 		driver.quit();
+		
 	}
 }
