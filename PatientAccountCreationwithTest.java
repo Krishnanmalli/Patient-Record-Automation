6fa253b2eccba7acb1380e2b6bc0cdca84d36e66
Krishnanmalli <mallikrishnan125@gmail.com>
2025-06-13 21:39:02 +0530

    Update and rename PatientaccountcreationwithTest.java to PatientAccountCreationwithTest.java

--- PatientAccountCreationwithTest.java
+++ PatientAccountCreationwithTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class PatientaccountcreationwithTest {
+public class PatientAccountCreationwithTest {
 
 	WebDriver driver;
 
